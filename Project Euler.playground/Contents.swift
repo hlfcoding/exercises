c9279ff8cf51ebd17adf1b273024935553213bfe
Peng Wang <peng@pengxwang.com>
2015-10-27 23:15:28 -0700

    Initial problem 7 solver with documentation
    Also get ready for problem 8.

--- Project Euler.playground/Contents.swift
+++ Project Euler.playground/Contents.swift
@@ -244,3 +244,67 @@ assert(squareOfSum(1...10) - sumOfSquares(1...10) == 2640)
 //:
 //: ---
 //:
+//: This is the slowest solver yet. Even with the sortedPrimes cache and the notion that primes are found by checking if they're divisible by previous primes, the outerloop runs 100k+ times, so the inner loop and the true operations count is probably much larger. However, the loop didn't seem to slow down very much as the numbers got bigger, so the approach here isn't O(n^2).
+//:
+//: Also, tick counts and printing are turned off to reduce operations. Any optimization I try to make in the outer loop with slicing sortedPrimes actually makes things run slower, probably due to compiler optimizations.
+//:
+var sortedPrimes = [2]
+func nthPrime(ordinal: Int) -> Int {
+    let prime: Int!
+    //var tick = 0
+    if ordinal > sortedPrimes.count {
+        var candidate = sortedPrimes.last!
+        outer: repeat {
+            candidate++
+            for p in sortedPrimes {
+                //tick++
+                if candidate % p == 0 {
+                    continue outer
+                }
+            }
+            // Is not divisible by previous primes.
+            sortedPrimes.append(candidate)
+            //print("\(tick): Added \(sortedPrimes.count)th prime: \(candidate)")
+        } while sortedPrimes.count < ordinal
+        prime = sortedPrimes.last!
+    } else {
+        prime = sortedPrimes[ordinal - 1]
+    }
+    return prime
+}
+
+assert(nthPrime(1) == 2)
+assert(nthPrime(6) == 13)
+// Wow.
+//nthPrime(10001)
+//:
+//: ## 8. Largest product in a series
+//:
+//: The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
+//:
+//:     73167176531330624919225119674426574742355349194934
+//:     96983520312774506326239578318016984801869478851843
+//:     85861560789112949495459501737958331952853208805511
+//:     12540698747158523863050715693290963295227443043557
+//:     66896648950445244523161731856403098711121722383113
+//:     62229893423380308135336276614282806444486645238749
+//:     30358907296290491560440772390713810515859307960866
+//:     70172427121883998797908792274921901699720888093776
+//:     65727333001053367881220235421809751254540594752243
+//:     52584907711670556013604839586446706324415722155397
+//:     53697817977846174064955149290862569321978468622482
+//:     83972241375657056057490261407972968652414535100474
+//:     82166370484403199890008895243450658541227588666881
+//:     16427171479924442928230863465674813919123162824586
+//:     17866458359124566529476545682848912883142607690042
+//:     24219022671055626321111109370544217506941658960408
+//:     07198403850962455444362981230987879927244284909188
+//:     84580156166097919133875499200524063689912560717606
+//:     05886116467109405077541002256983155200055935729725
+//:     71636269561882670428252483600823257530420752963450
+//:
+//: Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
+//:
+//: ---
+//:
+assert(true)
