8bf7352010251d7eb27d7be6b641aa7b15413b85
Peng Wang <peng@pengxwang.com>
2016-01-29 01:01:03 -0800

    Continue ES6 port of existing solutions

--- js-mirror/contents.js
+++ js-mirror/contents.js
@@ -222,3 +222,35 @@ function smallestMultipleDivisibleByRange(range, debug = false) {
 
 console.assert(smallestMultipleDivisibleByRange(createRange(10, 1)) === 2520, 'smallestMultipleDivisibleByRange');
 //console.log(smallestMultipleDivisibleByRange(createRange(20, 1)));
+
+// ## 6. Sum square difference
+//
+// The sum of the squares of the first ten natural numbers is 1^2 + 2^2 + ... +
+// 10^2 = 385
+//
+// The square of the sum of the first ten natural numbers is (1 + 2 + ... +
+// 10)^2 = 55^2 = 3025
+//
+// Hence the difference between the sum of the squares of the first ten natural
+// numbers and the square of the sum is 3025 − 385 = 2640.
+//
+// Find the difference between the sum of the squares of the first one hundred
+// natural numbers and the square of the sum.
+//
+// ---
+//
+// This problem can simply be brute-forced. The largest number isn't that big,
+// smaller than max 32-bit int.
+
+function sumOfSquaresForRange(range) {
+  return range.reduce((sum, n) => { return sum + Math.pow(n, 2); }, 0);
+}
+
+function squareOfSumOfRange(range) {
+  return Math.pow(range.reduce((sum, n) => { return sum + n; }, 0), 2);
+}
+
+console.assert(sumOfSquaresForRange(createRange(10, 1)) == 385, 'sumOfSquaresForRange');
+console.assert(squareOfSumOfRange(createRange(10, 1)) == 3025, 'squareOfSumOfRange');
+console.assert(squareOfSumOfRange(createRange(10, 1)) - sumOfSquaresForRange(createRange(10, 1)) == 2640, 'squareOfSumOfRange - sumOfSquaresForRange');
+//console.log(squareOfSumOfRange(createRange(100, 1)) - sumOfSquaresForRange(createRange(100, 1)));
